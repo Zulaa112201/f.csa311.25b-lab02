81259eae1c2a2aebe07899ae3b98ac8c7873f0d0
Zulaa112201 <_@.com>
2026-03-04 12:38:37 +0800

    zaswar

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,21 @@
 package edu.cmu.cs.cs214.lab02;
 
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
+import edu.cmu.cs.cs214.lab02.shapes.Square;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape tegshontsogt = new Rectangle(23, 11);
+        Shape booronhii= new Circle(6);
+        Shape kwadrat = new Square(5);
 
-        Renderer renderer = new Renderer(rectangle);
-
+        Renderer renderer = new Renderer(tegshontsogt);
         renderer.draw();
+        Renderer renderer2 = new Renderer(booronhii);
+        renderer2.draw();
+        Renderer renderer3 = new Renderer(kwadrat);
+        renderer3.draw();
     }
 }
